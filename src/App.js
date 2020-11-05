538fbdac830de2fbdaad8777fb08671161a8c4bb
Mitch Hansen <mitchames@live.com>
2020-11-04 17:20:15 -0800

    Added router and test data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import About from './components/About';
+import Navigation from './components/Navigation/Navigation';
+import HomePage from './components/HomePage';
+import UserDetails from './test-data/UserDetails';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
-    </div>
+    <body>
+        {/* create the Router to be able to change between pages seamlessly*/}
+        <HashRouter basename={"/"}>
+          <div>
+            <Navigation />
+            <Switch>
+              <Route path = "/" component = {HomePage} exact/>
+              <Route path = "/about" component = {About}/>
+              <Route path = "/user" component = {UserDetails}/>
+            </Switch>
+          </div>
+        </HashRouter>
+        
+      </body>
   );
 }
 
 export default App;
+
