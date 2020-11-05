0d04993d42a6b498593ee55c6fc67a8dfb32ce64
mitchahan <mitchames@live.com>
2020-11-05 11:33:51 -0800

    Added images, fixed some CSS

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,18 +8,18 @@ import {NavLink} from 'react-router-dom'
 import './Navigation.css'
 
 const Navigation = () => {
-    return(
-            <div className="menu">
-                <div clasName = "table">
-                    <ul className = "hor_lst">
-                        <li><NavLink to="/"exact>Home</NavLink></li>
-                        <li><NavLink to="/about">About</NavLink></li>
-			            <li><NavLink to="/user">User Profile</NavLink></li>
-                        <li><NavLink to="/products">Products</NavLink></li>
-                    </ul>
-                </div>
+    return (
+        <div className="menu">
+            <div clasName = "table">
+                <ul className = "hor_lst">
+                    <li><NavLink to="/"  exact>Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/user">User Profile</NavLink></li>
+                    <li><NavLink to="/products">Products</NavLink></li>
+                </ul>
             </div>
-    )
+        </div>
+    );
 }
 
 export default Navigation
--- src/test-data/ProductDetails.js
+++ src/test-data/ProductDetails.js
@@ -20,7 +20,7 @@ const testProducts = [
                     Blandit turpis cursus in hac habitasse platea. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Eget 
                     dolor morbi non arcu risus quis varius quam. Dictum sit amet justo donec. Dui faucibus in ornare quam viverra orci 
                     sagittis. Sit amet volutpat consequat mauris nunc.`,
-        photo: 'Null'
+        photo: 'https://www.bookbaby.com/Images/textbook-thumb-largeportrait.png'
     },
     {
         product_id: 2,
@@ -34,7 +34,21 @@ const testProducts = [
                     Blandit turpis cursus in hac habitasse platea. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Eget 
                     dolor morbi non arcu risus quis varius quam. Dictum sit amet justo donec. Dui faucibus in ornare quam viverra orci 
                     sagittis. Sit amet volutpat consequat mauris nunc.`,
-        photo: 'Null'
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/91CeJYT8uaL._AC_SX522_.jpg'
+    },
+    {
+        product_id: 3,
+        name: 'Table',
+        price: 15.00,
+        description: `This is a Table to put stuff on. Now here's some lorem ipsum to make it adequete:
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Cursus euismod quis viverra nibh cras. Nulla facilisi cras fermentum odio eu feugiat. 
+                    Accumsan tortor posuere ac ut consequat semper viverra nam. Quis risus sed vulputate odio ut enim blandit volutpat. 
+                    Commodo sed egestas egestas fringilla phasellus faucibus. Tempus imperdiet nulla malesuada pellentesque. 
+                    Blandit turpis cursus in hac habitasse platea. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Eget 
+                    dolor morbi non arcu risus quis varius quam. Dictum sit amet justo donec. Dui faucibus in ornare quam viverra orci 
+                    sagittis. Sit amet volutpat consequat mauris nunc.`,
+        photo: 'https://www.ikea.com/us/en/images/products/ingo-table-pine__0737092_PE740877_S5.JPG'
     }
 ];
 
@@ -43,9 +57,9 @@ const Product = props => {
 		<div>
 			<p>Product Id: {props.product_id}</p>
 			<p>Name: {props.name}</p>
-			<p>Price: {props.price}</p>
+			<p>Price: ${props.price}</p>
 			<p>Description: {props.description}</p>
-			<p>Photo: {props.photo}</p>
+            <img src={props.photo} height="200px" width="200px" alt={props.name} />
 		</div>
 	);
 };
@@ -74,6 +88,13 @@ const ProductDetails = props => {
                 price = {testProducts[1].price} 
                 description = {testProducts[1].description} 
                 photo = {testProducts[1].photo} 
+            />
+            <Product 
+                product_id = {testProducts[2].product_id} 
+                name = {testProducts[2].name} 
+                price = {testProducts[2].price} 
+                description = {testProducts[2].description} 
+                photo = {testProducts[2].photo} 
             />
 		</div>
 	);
