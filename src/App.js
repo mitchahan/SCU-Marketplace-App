ca5927b30ce04db6175d7a0a528b13a4e07b02f0
mitchahan <mitchames@live.com>
2020-11-06 14:06:24 -0800

    Fixed navbar and added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.scss';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import About from './components/About';
 import Navigation from './components/Navigation/Navigation';
@@ -7,14 +6,17 @@ import UserDetails from './test-data/UserDetails';
 import ProductDetails from './test-data/ProductDetails';
 import Login from './components/Login Page/Login';
 import Register from './components/Login Page/Register';
+import './App.scss';
 
 function App() {
   return (
     <body>
         {/* Create the Router to be able to change between pages seamlessly*/}
         <HashRouter basename={"/"}>
-          <div>
+          <div className="nav-container">
             <Navigation />
+          </div>
+          <div className="body-container">
             <Switch>
               <Route path = "/" component = {HomePage} exact/>
               <Route path = "/about" component = {About}/>
