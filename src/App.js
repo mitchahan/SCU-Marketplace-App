fcddb0973d539fca154b8f6f7dfa992b5b9304f8
glang <glang@DESKTOP-D8JMETG>
2020-11-06 12:56:30 -0800

    please review mitch daddy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import About from './components/About';
 import Navigation from './components/Navigation/Navigation';
