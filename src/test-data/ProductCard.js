8580927952be1f897da06ba26dc3578966d0a189
Paul Mason <pcmason@scu.edu>
2020-11-13 14:42:21 -0800

    Create ProductCard.js

---
+++ src/test-data/ProductCard.js
@@ -0,0 +1,24 @@
+/* @author Mitch Hansen
+ * @lastModifiedBy Mitch Hansen
+ * Description: This file is the test data for a couple of product items. Until we've
+ * connected the DB, this will serve as data to populate.
+ */
+
+import React from 'react'
+
+export default function ProductCard(props) {
+    const product = props.product;
+    return (
+        <div>
+            <img
+                src = {product.photo}
+                alt = {product.name + 'picture'}
+            />
+            <div>
+                <h3>{product.name}</h3>
+                <p> Price: ${product.price}</p>
+                <p> Description: {product.description}
+            </div>
+        </div>
+    )
+}
