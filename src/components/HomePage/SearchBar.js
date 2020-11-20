24c1afb0a8c2d5bcc3b259ee27577144fbdadcbc
glangsdorf <57603556+glangsdorf@users.noreply.github.com>
2020-11-19 16:51:52 -0800

    made a search button
    made a search button

--- src/components/HomePage/SearchBar.js
+++ src/components/HomePage/SearchBar.js
@@ -1,5 +1,5 @@
 /* @author Beau Burnight
- * @lastModifiedBy Beau Burnight
+ * @lastModifiedBy Griffin Langsdorf
  * Description: This the homepage. Expected functionality includes the nav bar, a search par for producs, and a button to create a new listing.
  */
 
@@ -10,13 +10,17 @@ const SearchBar = ({input:keyword,onChange:setKeyword}) => {
   const BarStyling = {width:"40rem", backround:"#F2F1F9", border:"solid", padding:".5rem", float:"left"};
   return(
     //search bar
-    <input style={BarStyling}
-    key="random1"
-    value= {keyword}
-    placeholder={"Search Product"}
-    //onChange={(e) => setKeyword(e.target.value)}
-    />
-
+    <div className = "base-container">
+      <input style={BarStyling}
+      key="random1"
+      value= {keyword}
+      placeholder={"Search Product"}
+      //onChange={(e) => setKeyword(e.target.value)}
+      />
+      <div className = "searchButton">
+        <button type = "button" className="btn" float="right">Search</button>
+      </div>
+    </div>
   )
 }
 
