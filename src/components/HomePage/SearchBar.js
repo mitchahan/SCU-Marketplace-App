0fbfccacd529efe33c79c10207057af170e410c4
Cooper Smith <cooper.smith0808@gmail.com>
2020-11-29 14:40:56 -0800

    About page
    made about page look better (can't figure out how to make images resize based on the page size)

---
+++ src/components/HomePage/SearchBar.js
@@ -0,0 +1,23 @@
+/* @author Beau Burnight
+ * @lastModifiedBy Beau Burnight
+ * Description: This the homepage. Expected functionality includes the nav bar, a search par for producs, and a button to create a new listing.
+ */
+
+import React from 'react'
+import ProductCard from './ProductCard.js';
+
+const SearchBar = ({input:keyword,onChange:setKeyword}) => {
+  const BarStyling = {width:"40rem", backround:"#F2F1F9", border:"solid", padding:".5rem", float:"left"};
+  return(
+    //search bar
+    <input style={BarStyling}
+    key="random1"
+    value= {keyword}
+    placeholder={"Search Product"}
+    //onChange={(e) => setKeyword(e.target.value)}
+    />
+
+  )
+}
+
+export default SearchBar
